1cb71b401614c703f0cea30b4d856f7ddb984e70
Gabriel Huau <gabriel.huau@innopeaktech.com>
2020-09-18 09:56:01 -0700

    docker: build renderdoc as static library
    This commits moves the build system closer to what RenderDoc is doing
    for the official binaries online, meaning building QT5/Python as static
    libraries.
    
    We don't care about INTERCEPTOR in our cases so we can avoid this
    dependencies too (we use the debug layers)

--- docker/build.sh
+++ docker/build.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
+set -e
+
 CURRENT_PATH="$(dirname "$(readlink -f "$0")")"
+
+cd $CURRENT_PATH
 
 USER_ID="$(id -u)"
 GROUP_ID="$(id -g)"
 
-docker build --rm -t jedi-renderdoc -<<EOF
-FROM ubuntu:18.04
+docker build --rm -t jedi-renderdoc -f- . <<EOF
+FROM ubuntu:xenial
 
 ARG DEBIAN_FRONTEND=noninteractive
 
-RUN apt-get update && \
-    apt-get -y dist-upgrade && \
-    apt-get install -qqy zlib1g-dev curl unzip wget python libc++1 openjdk-8-jdk libproxy1v5 git-lfs python \
-                       cmake libqt5x11extras5-dev qt5-default qt5-qmake libqt5svg5-dev automake libpcre3-dev \
-                       python3-dev python3-pip python3-tk python3-lxml python3-six libxcb-keysyms1-dev pkg-config \
-                       && \
-    apt-get clean
-
 RUN groupadd -r $USER -g $GROUP_ID  && adduser --system --GID $GROUP_ID --UID $USER_ID $USER
 RUN echo "docker" > /etc/debian_chroot
+
+COPY prepare.sh /
+COPY static_tagging.patch /
+RUN /prepare.sh
 
 USER $USER
 
@@ -28,9 +28,4 @@ EOF
 
 docker run -it --rm --network host -v "$CURRENT_PATH/../":/jedi/ jedi-renderdoc
 
-RENDERDOC_LIB_PATH="$(dirname "$(readlink -f "$0")")/../build-python/lib/"
-echo "########################################################"
-echo "TO USE RENDERDOC, PLEASE RUN THE FOLLOWING COMMANDS:"
-echo "$ export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$RENDERDOC_LIB_PATH"
-echo "$ export PYTHONPATH=\$PYTHONPATH:$RENDERDOC_LIB_PATH"
-echo "########################################################"
+cd -
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,9 +2,39 @@
 
 set -e
 
+cd /jedi/
+mkdir -p renderdoc_build
+cd renderdoc_build
+CC=clang CXX=clang++ CFLAGS="-fPIC -fvisibility=hidden" cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/jedi/dist/ -DVULKAN_LAYER_FOLDER=/jedi/dist/etc/vulkan/implicit_layer.d -DSTATIC_QRENDERDOC=ON -DQRENDERDOC_NO_CXX11_REGEX=ON ../
+make -j8
+make install
+
+# Copy python modules
+mkdir -p /jedi/pymodules
+cp -R lib/*.so /jedi/pymodules
+
+# Copy python lib folder, and trim
+mkdir -p /jedi/dist/share/renderdoc/pylibs/lib
+cd /jedi/dist/share/renderdoc/pylibs/lib
+cp -R /usr/lib/python3.6/ .
+cd python3.6
+# remove cache files
+rm -rf $(find -iname __pycache__)
+# remove unwanted modules
+rm -rf test site-packages ensurepip distutils idlelib config-*
+
+cd /jedi/
+
+# Strip the binaries
+strip --strip-unneeded dist/bin/*
+strip --strip-unneeded dist/lib/*
+
+# Build android libraries and apks
 export ANDROID_HOME=/jedi/android-sdk-linux
 export ANDROID_NDK_HOME=/jedi/android-sdk-linux/ndk-bundle/
+export ANDROID_SDK=$ANDROID_HOME/
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=$PATH:$ANDROID_SDK/tools/
 
 if [ ! -d "$ANDROID_HOME" ]
 then
@@ -18,22 +48,37 @@ then
   echo "Installing android tools...done"
 fi
 
-cd /jedi/
+# Check that we're set up to build for android
+if [ ! -d $ANDROID_SDK/tools ] ; then
+	echo "\$ANDROID_SDK is not correctly configured: '$ANDROID_SDK'"
+	exit 0;
+fi
 
-mkdir -p build-python
-cd build-python
-cmake -DENABLE_PYRENDERDOC=1 -DENABLE_GL=Off -DENABLE_XLIB=Off -DENABLE_XCB=Off -DENABLE_VULKAN=Off ..
+# Build the arm32 variant
+rm -rf build-android-arm32
+mkdir build-android-arm32
+pushd build-android-arm32
+
+cmake -DBUILD_ANDROID=1 -DANDROID_ABI=armeabi-v7a -DANDROID_NATIVE_API_LEVEL=23 -DCMAKE_BUILD_TYPE=Release -DSTRIP_ANDROID_LIBRARY=On -DCMAKE_MAKE_PROGRAM=make ..
 make -j8
 
-cd -
-mkdir -p build-android-arm32
-cd build-android-arm32
-cmake -DBUILD_ANDROID=1 -DANDROID_ABI=armeabi-v7a -DANDROID_NATIVE_API_LEVEL=23 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=make ..
-make -j8
-cd -
+if ! ls bin/*.apk; then
+  echo "Android build failed"
+  exit 0;
+fi
 
-mkdir -p build-android-arm64
-cd build-android-arm64
-cmake -DBUILD_ANDROID=1 -DANDROID_ABI=arm64-v8a -DANDROID_NATIVE_API_LEVEL=23 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=make ..
+popd # build-android-arm32
+
+rm -rf build-android-arm64
+mkdir build-android-arm64
+pushd build-android-arm64
+
+cmake -DBUILD_ANDROID=1 -DANDROID_ABI=arm64-v8a -DANDROID_NATIVE_API_LEVEL=23 -DCMAKE_BUILD_TYPE=Release -DSTRIP_ANDROID_LIBRARY=On -DCMAKE_MAKE_PROGRAM=make ..
 make -j8
-cd -
+
+if ! ls bin/*.apk; then
+  echo "Android build failed"
+  exit 0;
+fi
+
+popd # build-android-arm64
